4f6fc824b857e707989efc2affca9bdfc36b8c56
Varun NAGA <varun.naga@conduent.com>
2020-11-09 01:13:15 +0400

    Now using DOM traversal.Added several more conversions.

--- src/App.js
+++ src/App.js
@@ -3,80 +3,172 @@ import React, {Component} from 'react';
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.rootNode = React.createRef();
     this.state = {
-      celsiusVal : 0,
-      fahrenheitVal : 32
+      formulas: {
+        "default" : (value) => value,
+        "Celsius-Fahrenheit" : (value) => value * 1.8 + 32,
+        "Fahrenheit-Celsius" : (value) => value / 1.8 - 32,
+        "Kelvin-Celsius" : (value) => value - 273.15,
+        "Celsius-Kelvin" : (value) => value + 273.15,
+        "Kelvin-Fahrenheit" : (value) => (value-273.15) * 1.8 + 32,
+        "Fahrenheit-Kelvin" : (value) => (value-32) / 1.8 + 273.15,
+        "inch-cm" : (value) => value * 2.54,
+        "cm-inch" : (value) => value / 2.54,
+        "kg-pound" : (value) => value * 2.20462,
+        "pound-kg" : (value) => value / 2.20462,
+        "calorie-joule" : (value) => value * 4.184,
+        "joule-calorie" : (value) => value / 4.184,
+        "psi-pascal" : (value) => value * 6894.76,
+        "pascal-psi" : (value) => value / 6894.76
+      },
     };
   }
 
   handleInputChange= (event)=>{
-    var elementId = event.target.id;
-    var floatVal = parseFloat(event.target.value);
+    // Retrieve the input and select elements
+    var cellColumn = event.target.parentElement;
+    var cellRow = cellColumn.parentElement;
+    
+    var columnIndex = -1;
+    cellRow.childNodes.forEach((currentValue, currentIndex, listObj) => {
+      if (currentValue === cellColumn)
+        columnIndex = currentIndex;
+    });
 
-    if (elementId === "celsiusInput") {
-      this.setState({celsiusVal: floatVal, fahrenheitVal: floatVal * 1.8 + 32 });
-      console.log(this.state.fahrenheitVal.toString());
-    } else if (elementId === "fahrenheitInput") {
-      this.setState({ celsiusVal: (floatVal - 32)/1.8, fahrenheitVal:floatVal});
+
+    var inputElementA = null, inputElementB = null, selectNodeA = null, selectNodeB = null;
+    switch(columnIndex)
+    {
+      case 0:
+      case 3:
+        inputElementA = cellRow.childNodes[0].childNodes[0];
+        inputElementB = cellRow.childNodes[2].childNodes[0];
+        selectNodeA = cellRow.childNodes[1].childNodes[0];
+        selectNodeB = cellRow.childNodes[3].childNodes[0];
+        break;
+      case 1:
+      case 2:
+        inputElementA = cellRow.childNodes[2].childNodes[0];
+        inputElementB = cellRow.childNodes[0].childNodes[0];
+        selectNodeA = cellRow.childNodes[3].childNodes[0];
+        selectNodeB = cellRow.childNodes[1].childNodes[0];
+        break;
+      default:
+        break;
+    }
+
+    var formulaIndex = "default";
+
+    if (selectNodeA.value !== selectNodeB.value) {
+      formulaIndex = `${selectNodeA.value}-${selectNodeB.value}`;
+    }
+
+    var sourceValue = parseFloat(inputElementA.value);
+    inputElementB.value = (this.state.formulas[formulaIndex](sourceValue)).toString();
+  }
+
+  componentDidMount() {
+    // Populate the input elements
+    let seedValue = 0;
+    for (var index = 1; index <= this.rootNode.current.childNodes.length - 1; ++index) {
+      let firstInputElement = this.rootNode.current.childNodes[index].childNodes[0].childNodes[0];
+      let firstSelectElement = this.rootNode.current.childNodes[index].childNodes[1].childNodes[0];
+      let secondInputElement = this.rootNode.current.childNodes[index].childNodes[2].childNodes[0];
+      let secondSelectElement = this.rootNode.current.childNodes[index].childNodes[3].childNodes[0];
+      
+      let formulaIndex = `${firstSelectElement.value}-${secondSelectElement.value}`;
+      firstInputElement.setAttribute("value", seedValue.toString());
+      secondInputElement.setAttribute("value", (this.state.formulas[formulaIndex](seedValue)).toString());
     }
   }
 
   render = ()=>
-    <div className="container-fluid p-4">
+    <div className="container-fluid p-4" ref={this.rootNode}>
       <div className="row bg-info text-white p-2">
         <div className="col-12 font-weight-bold text-center">Units converter</div>
       </div>
       <div className="row bg-light p-2 border">
-        <div className="col"><input type="Number" id="celsiusInput" onChange={this.handleInputChange} value={this.state.celsiusVal.toString()} /></div>
-        <div className="col">Celsius</div>
-        <div className="col"><input type="Number" id="fahrenheitInput" onChange={this.handleInputChange} value={this.state.fahrenheitVal.toString()}/></div>
-        <div className="col">Fahrenheit</div>
-      </div>
-      <div className="row bg-light p-2 border">
-        <div className="col"><input type="Number" /></div>
+        <div className="col"><input type="Number" onChange={this.handleInputChange} /></div>
         <div className="col">
-          <select class="browser-default custom-select">
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+          <select className="browser-default custom-select" onChange={this.handleInputChange}>
+            <option>Celsius</option>
+            <option>Fahrenheit</option>
+            <option>Kelvin</option>
           </select>
         </div>
-        <div className="col"><input type="Number" /></div>
+        <div className="col"><input type="Number" onChange={this.handleInputChange} /></div>
         <div className="col">
-          <select class="browser-default custom-select">
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+          <select className="browser-default custom-select" onChange={this.handleInputChange}>
+            <option>Kelvin</option>
+            <option>Fahrenheit</option>
+            <option>Celsius</option>
           </select>
-        </div>        
+        </div>
       </div>
+      <div className="row bg-light p-2 border">
+        <div className="col"><input type="Number" onChange={this.handleInputChange} /></div>
+        <div className="col">
+          <select className="browser-default custom-select" onChange={this.handleInputChange}>
+            <option>inch</option>
+            <option>cm</option>
+          </select>
+        </div>
+        <div className="col"><input type="Number" onChange={this.handleInputChange} /></div>
+        <div className="col">
+          <select className="browser-default custom-select" onChange={this.handleInputChange}>
+            <option>cm</option>
+            <option>inch</option>
+          </select>
+        </div>
+      </div>
+      <div className="row bg-light p-2 border">
+        <div className="col"><input type="Number" onChange={this.handleInputChange} /></div>
+        <div className="col">
+          <select className="browser-default custom-select" onChange={this.handleInputChange}>
+            <option>kg</option>
+            <option>pound</option>
+          </select>
+        </div>
+        <div className="col"><input type="Number" onChange={this.handleInputChange} /></div>
+        <div className="col">
+          <select className="browser-default custom-select" onChange={this.handleInputChange}>
+            <option>pound</option>
+            <option>kg</option>
+          </select>
+        </div>
+      </div>
+      <div className="row bg-light p-2 border">
+        <div className="col"><input type="Number" onChange={this.handleInputChange} /></div>
+        <div className="col">
+          <select className="browser-default custom-select" onChange={this.handleInputChange}>
+            <option>calorie</option>
+            <option>joule</option>
+          </select>
+        </div>
+        <div className="col"><input type="Number" onChange={this.handleInputChange} /></div>
+        <div className="col">
+          <select className="browser-default custom-select" onChange={this.handleInputChange}>
+            <option>joule</option>
+            <option>calorie</option>
+          </select>
+        </div>
+      </div>
+      <div className="row bg-light p-2 border">
+        <div className="col"><input type="Number" onChange={this.handleInputChange} /></div>
+        <div className="col">
+          <select className="browser-default custom-select" onChange={this.handleInputChange}>
+            <option>psi</option>
+            <option>pascal</option>
+          </select>
+        </div>
+        <div className="col"><input type="Number" onChange={this.handleInputChange} /></div>
+        <div className="col">
+          <select className="browser-default custom-select" onChange={this.handleInputChange}>
+            <option>pascal</option>
+            <option>psi</option>
+          </select>
+        </div>
+      </div>                       
     </div>
 }
-
-/*import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
